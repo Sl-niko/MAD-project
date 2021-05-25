d8c9c733432bcfb9f62ecff5f874dcea17f418d0
Sl-niko <nipunpesala1996@gmail.com>
2021-05-26 00:10:59 +0530

    Final project

---
+++ app/src/main/java/com/example/madmini/ui/gallery/GalleryFragment.java
@@ -0,0 +1,34 @@
+package com.example.madmini.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.madmini.R;
+
+public class GalleryFragment extends Fragment {
+
+    EditText firstName,LastName,Mobile,Email;
+    private GalleryViewModel galleryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(GalleryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        return root;
+    }
+
+
+}
